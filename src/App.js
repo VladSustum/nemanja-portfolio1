e022c15db464b09ad92616ab2bad5629b03f5f8d
Vlad <dowlabre@gmail.com>
2021-01-06 02:52:30 +0100

    fixing stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {HashRouter as Router, Switch, Route, useLocation, HashRouter, NavLink} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
-import Portfolio from './components/Portfolio';
+import Portfolio from './components/pages/Portoflio';
 import Onama from './components/Onama'
 import Kontakt from './components/Kontakt/Kontakt';
 import Usluge from './components/Usluge';
@@ -28,8 +28,8 @@ function App() {
        <Route path='/onama'  component={Onama} />
        <Route path='/kontakt' component={Kontakt} />
        <Route path='/usluge' component={Usluge} />
-       <Route path='*' component={NoMatch} ></Route>
-     </Switch>
+       <Route path='*' component={NoMatch} />
+       </Switch>
      </HashRouter>
      </>
   );
