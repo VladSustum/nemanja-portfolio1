93e6b8364732680dbddc9a0d625bfc487a6e8278
Vlad <dowlabre@gmail.com>
2021-01-07 22:55:38 +0100

    fixing stuff

---
+++ src/components/pages/Onama.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import '../../App.css';
+import Onama from '../Onama/Onama'
+import { homeObjOne } from '../Onama/OnamaData';
+
+function Onama(){
+    return 
+    <>
+    <Onama {...homeObjOne} />
+    
+    </>
+
+export default Onama; 
+}
