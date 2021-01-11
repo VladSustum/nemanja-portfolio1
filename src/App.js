3ac9498e886df27943ea541b61bad994b64ac95c
Vlad <dowlabre@gmail.com>
2021-01-11 18:41:44 +0100

    fixing stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import react from 'react';
 import './App.css';
-import {HashRouter as Router, Switch, Route, useLocation, HashRouter, NavLink} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, useLocation, HashRouter, NavLink} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Portfolio from './components/Portfolio';
@@ -20,7 +20,7 @@ const NoMatch = () => {
 function App() {
   return (
     <>
-    <HashRouter>
+    <Router>
      <Navbar />
      <Switch>
        <Route path='/' exact component=
@@ -31,7 +31,7 @@ function App() {
        <Route path='/usluge' component={Usluge} />
        <Route path='*' component={NoMatch} />
        </Switch>
-     </HashRouter>
+     </Router>
      </>
   );
 }
