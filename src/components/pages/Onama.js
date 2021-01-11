36d9c4b5a29c6a779073d6f56a7167df945d3631
Vlad <dowlabre@gmail.com>
2021-01-11 19:54:53 +0100

    fixing stuff

--- src/components/pages/Onama.js
+++ src/components/pages/Onama.js
@@ -1,13 +1,20 @@
 import react from 'react';
 import '../../App.css';
+import Footer from '../Footer';
+import Kontakt from '../Kontakt/Kontakt';
 import Onama from '../Onama/Onama'
 import { homeObjOne } from '../Onama/OnamaData';
+import Portfolios from '../Portfolios';
+import Usluge from '../Usluge';
 
 function Onama(){
     return 
     <>
     <Onama {...homeObjOne} />
-    
+    <Usluge></Usluge>
+    <Portfolio></Portfolio>
+    <Kontakt></Kontakt>
+    <Footer></Footer>
     </>
 
 
