843613bcc206d0a64c8f91d2876c72bad6a73704
Vlad <dowlabre@gmail.com>
2021-01-12 19:49:17 +0100

    fixing stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,9 +21,9 @@ function Navbar(){
     <>
     <nav className='navbar'>
     <div className='navbar-container'>
-      <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+      <NavLink to='/' className='navbar-logo' onClick={closeMobileMenu}>
          FAMEMPIRE <i className='fab fa-type3'></i>
-          </Link>  
+          </NavLink>  
          <div className="menu-icon" onClick={handleClick}>
              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
          </div>
