84a24602379773458fea1f16a8cbe4df3841b530
Vlad <dowlabre@gmail.com>
2021-01-16 18:25:13 +0100

    fixing stuff

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,10 +6,10 @@ import '../App.css';
 
 function HeroSection(){
     return (
-<div className='hero-container'> 
+<div className='hero-container'>    
 <h1>FAMEMPIRE</h1>
-<h2>MARKENTISKA KOMPANIJA</h2>      
-<p>Vas uspeh je i nas uspeh</p>
+<h2>MARKENTISKA KOMPANIJA</h2>
+<p><b>"Vas uspeh je i nas uspeh"</b></p>
 <div className='hero-btns'>
     <Button className='btns' buttonStyle="btn--outline" buttonSize='btn--large'>
         Kontakt
